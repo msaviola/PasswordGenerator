eb462c485a8baa73ceb7ff526bc94d76769d0dcb
Mariella Saviola <msaviola@sandiego.edu>
2020-07-19 16:57:25 -0700

    working and readme

--- script.js
+++ script.js
@@ -17,13 +17,16 @@ generateBtn.addEventListener("click", function () {
     var lower ="abcdefghijklmnopqrstuvwxyz";
     var specialchar ="#@!%&()/";
     var numbers = "0123456789";
-   
+
+
+   while(true){
     var length = prompt("please enter a value between 8-128","length");   
     var randPwLen= parseInt(length);
 
     if(randPwLen<8 || randPwLen>128){
       alert("Invalid password length, try again")
       break;
+      
     }
 
    
@@ -44,56 +47,34 @@ generateBtn.addEventListener("click", function () {
       passwordText;
       };
 
+
+    const useSpecialchar = confirm('Do you want special characters?');
+      if (useSpecialchar) {
+        passwordText = passwordText.concat(specialchar);
+        }
+  
+      else{
+        passwordText;
+        };
+
+    const useNumbers = confirm('Do you want numbers?');
+        if (useNumbers) {
+          passwordText = passwordText.concat(numbers);
+          }
     
-      console.log(randPwLen);
+        else{
+          passwordText;
+          };  
+
+        
+
+    
+      //console.log(randPwLen);
       var randPassword = Array(randPwLen).fill(passwordText).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
       //return randPassword; 
-       console.log (randPassword);
+      //console.log (randPassword);
        document.getElementById('password').textContent=randPassword;
+       break;
   
-
+    }
 });
-
-// Add event listener to generate button for writepassword function
-
-
-//from glitch
-
-// const lowerCase = 'abcde'.split('');
-// const upperCase = 'ABCDE'.split('');
-
-// const useLowerCase = confirm('Do you want lower case letters?');
-// const useUpperCase = confirm('Do you want upper case letters?');
-
-// let password = [];
-
-// if (useLowerCase) {
-//     for (let i = 0; i < 2; i++) {
-//         password.push('randomCharacter');
-//     }
-// }
-
-// if (useUpperCase) {
-//     password = password.concat(upperCase);
-// }
-
-// console.log(shuffle(password).join(''));
-
-// function shuffle(array) {
-//     var currentIndex = array.length, temporaryValue, randomIndex;
-
-//     // While there remain elements to shuffle...
-//     while (0 !== currentIndex) {
-
-//         // Pick a remaining element...
-//         randomIndex = Math.floor(Math.random() * currentIndex);
-//         currentIndex -= 1;
-
-//         // And swap it with the current element.
-//         temporaryValue = array[currentIndex];
-//         array[currentIndex] = array[randomIndex];
-//         array[randomIndex] = temporaryValue;
-//     }
-
-//     return array;
-// }
